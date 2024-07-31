8127915639c8ba299f7b4ee7cefaaa69190bdb8b
Joselu <joseluissgz97@gmail.com>
2024-07-31 10:22:42 +0200

    chore: Add error handling middleware to handle API errors

--- prodigioso_volcan_api/src/app.ts
+++ prodigioso_volcan_api/src/app.ts
@@ -1,8 +1,10 @@
 import cors from 'cors';
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response } from 'express';
+
 import { conectDB } from './config/conectDB';
 import { loginRouter } from './routes/login.routes';
+import { ErrorApi } from './utils/erroApi';
 
 conectDB();
 export const expressApp = express();
@@ -20,3 +22,17 @@ expressApp.use(express.json());
 expressApp.use(express.text());
 
 expressApp.use(loginRouter);
+
+expressApp.use((err: Error, req: Request, res: Response) => {
+  if (err instanceof ErrorApi) {
+    return res.status(err.statusCode).json({
+      error: true,
+      message: err.message
+    });
+  } else {
+    res.status(500).json({
+      error: true,
+      message: err.message
+    });
+  }
+});
