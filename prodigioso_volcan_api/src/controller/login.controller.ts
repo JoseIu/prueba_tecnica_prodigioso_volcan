f637fd82239b13fd688887170eb5ec9a21cb6f1b
Joselu <joseluissgz97@gmail.com>
2024-07-31 10:22:50 +0200

    feat: Refactor login controller to use catchAsync utility and improve error handling

--- prodigioso_volcan_api/src/controller/login.controller.ts
+++ prodigioso_volcan_api/src/controller/login.controller.ts
@@ -3,10 +3,13 @@ import { OAuth2Client } from 'google-auth-library';
 import { jwtDecode } from 'jwt-decode';
 import { UserInterface } from '../interfaces/user.interface';
 import { User } from '../model/user.model';
+import { catchedAsyc } from '../utils/catchAsyc';
+import { ErrorApi } from '../utils/erroApi';
+import { responseApi } from '../utils/responseApi';
 
 const oAuthClient = new OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET, 'postmessage');
 
-export const loginGoogleController = async (req: Request, res: Response) => {
+const loginGoogleController = async (req: Request, res: Response) => {
   const { authorization } = req.headers;
 
   const token = authorization!.split(' ')[1];
@@ -17,52 +20,39 @@ export const loginGoogleController = async (req: Request, res: Response) => {
 
   //If not exit, create the user
   if (!existUser) {
-    try {
-      const newUser = new User(userDataByToken);
-      const newUserSaved = await newUser.save();
-      return res.status(201).json({ error: false, user: newUserSaved, message: 'Usuario logeado correctamente' });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: true, user: null, message: 'Internal Server Error' });
-    }
-  }
-
-  return res.status(200).json({ error: false, user: existUser, message: 'Usuario logeado correctamente' });
-};
-
-export const loginGoogleV2Controller = async (req: Request, res: Response) => {
-  const { code } = req.body;
-
-  if (!code) {
-    return res.status(400).json({
-      error: true,
-      message: 'Code is required'
+    const newUser = new User(userDataByToken);
+    const newUserSaved = await newUser.save();
+    return responseApi(res, 201, {
+      error: false,
+      user: newUserSaved,
+      message: 'Usuario logeado correctamente'
     });
   }
+  return responseApi(res, 200, { error: false, user: existUser, message: 'Usuario logeado correctamente' });
+};
 
-  try {
-    const { tokens } = await oAuthClient.getToken(code);
-    const { id_token } = tokens;
-    const tokedDecoded = jwtDecode(id_token!);
+const loginGoogleV2Controller = async (req: Request, res: Response) => {
+  const { code } = req.body;
 
-    const userDataByToken = tokedDecoded as UserInterface;
-    const existUser = await User.findOne({ sub: userDataByToken.sub });
+  if (!code) throw new ErrorApi('Code is required', 400);
 
-    if (!existUser) {
-      try {
-        const newUser = new User(userDataByToken);
-        const newUserSaved = await newUser.save();
-        return res
-          .status(201)
-          .json({ error: false, user: newUserSaved, message: 'Usuario logeado correctamente' });
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: true, user: null, message: 'Internal Server Error' });
-      }
-    }
-    res.status(200).json({ error: false, user: existUser, message: 'Usuario logeado correctamente' });
-  } catch (error) {
-    console.log(error);
-    res.status(200).json({ error: true, message: 'Token or ID invalid' });
+  const { tokens } = await oAuthClient.getToken(code);
+  const { id_token } = tokens;
+  const tokedDecoded = jwtDecode(id_token!);
+
+  const userDataByToken = tokedDecoded as UserInterface;
+  const existUser = await User.findOne({ sub: userDataByToken.sub });
+
+  if (!existUser) {
+    const newUser = new User(userDataByToken);
+    const newUserSaved = await newUser.save();
+
+    return responseApi(res, 201, { error: false, user: newUserSaved, message: 'Usuario logeado correctamente' });
   }
+  return responseApi(res, 200, { error: false, user: existUser, message: 'Usuario logeado correctamente' });
+};
+
+export default {
+  loginGoogleController: catchedAsyc(loginGoogleController),
+  loginGoogleV2Controller: catchedAsyc(loginGoogleV2Controller)
 };
