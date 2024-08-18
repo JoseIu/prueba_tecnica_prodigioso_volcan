c649796924f9be0c2bb219b463129ebce07f8308
Joselu <joseluissgz97@gmail.com>
2024-08-18 23:39:48 +0200

    Fix: fixing APIResponseInterface

--- prodigioso_volcan_front/src/interface/apiResponse.interface.ts
+++ prodigioso_volcan_front/src/interface/apiResponse.interface.ts
@@ -1,10 +1,15 @@
 export interface APIResponseInterface {
   error: boolean;
+  data: Data;
+}
+
+export interface Data {
   user: User;
   message: string;
 }
 
 export interface User {
+  _id: string;
   iss: string;
   sub: string;
   email: string;
@@ -12,5 +17,4 @@ export interface User {
   name: string;
   given_name: string;
   picture: string;
-  _id: string;
 }
